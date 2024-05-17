102695b78cb5de410738bad720d41fe7a128e48c
AliTigui <alitighiouart2001@gmail.com>
2024-05-18 00:15:10 +0200

    added source code

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+import uuid
+from flask import Flask,  redirect, render_template
+
+
+
+# Configure application
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+
+
+
