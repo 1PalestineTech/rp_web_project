b23c4093edf3a560e1cbab11ff30d20a29b8fc97
AliTigui <alitighiouart2001@gmail.com>
2024-08-11 03:47:29 +0200

    adds

--- app.py
+++ app.py
@@ -28,16 +28,16 @@ def about():
 def login():
     db = sqlite3.connect('web_data.db')
     if request.method == "POST":
-        username = request.form.get('username').strip()
-        password = request.form.get('password').strip()
+
         if not request.form.get("username"):
             return render_template("error.html", top=403, bottom="must provide username",url=request.path),403 
 
         elif not request.form.get("password"):
             return render_template("error.html", top=403, bottom="must provide password",url=request.path),403 
+        username = request.form.get('username').strip()
+        password = request.form.get('password').strip()
         cursor = db.execute("SELECT * FROM admins WHERE username = (?) ",(username,))
         rows = cursor.fetchall()
-        print(password+".")
         if len(rows) != 1 or not check_password_hash(rows[0][2],password):
             return render_template("error.html", top=403, bottom="invalid username and/or password",url=request.path),403 
         session["user_id"] = rows[0][0]
@@ -48,6 +48,31 @@ def login():
         else:
             return render_template("admin_login.html")
 
+@app.route('/change_password', methods = ['GET','POST'])
+@login_required
+def change_password():
+    db = sqlite3.connect('web_data.db')
+    if request.method == "POST":
+        if not request.form.get("old_password"):
+            return render_template("error.html", top=403, bottom="must provide old password",url=request.path),403 
+
+        elif not request.form.get("new_password"):
+            return render_template("error.html", top=403, bottom="must provide the new password",url=request.path),403 
+        old_password = request.form.get('old_password').strip()
+        new_password = request.form.get('new_password').strip()
+        cursor = db.execute("SELECT * FROM admins WHERE id = (?) ",(session.get("user_id"),))
+        rows = cursor.fetchall()
+
+        if len(rows) != 1 or not check_password_hash(rows[0][2],old_password):
+            return render_template("error.html", top=403, bottom="wrong password",url=request.path),403
+        hs=generate_password_hash(new_password) 
+        db.execute("UPDATE admins SET password = (?) WHERE id = (?)",(hs,session.get("user_id")))
+        db.commit()
+        
+        return redirect("/admin_control")
+    else :
+
+        return render_template("change_password.html")
 @app.route('/add_admin', methods = ['GET','POST'])
 @login_required
 @head_admin
@@ -94,6 +119,17 @@ def remove_admin():
 @app.route('/add_item', methods = ['POST'])
 @login_required
 def add_item():
+    if not request.form.get('type'):
+        return render_template("error.html", top=403, bottom="Select type",url=request.path),403 
+
+    elif not request.files.getlist("images"):
+        return render_template("error.html", top=403, bottom="pls provide 1 image or more",url=request.path),403 
+    elif not request.files['file'] :
+        return render_template("error.html", top=403, bottom="pls select the text file",url=request.path),403 
+    elif not request.form.get('title') :
+        return render_template("error.html", top=403, bottom="pls select the text file",url=request.path),403
+    elif not request.form.get('desciption') :
+        return render_template("error.html", top=403, bottom="pls provide desciption ",url=request.path),403
     id = str(shortuuid.ShortUUID(alphabet="013456789").random(length=20))
     dir=request.form.get('type').strip()
     parent_dir = "./static/"+dir
@@ -110,6 +146,12 @@ def add_item():
     with open( path2  ,'w') as fl:
         data=data.replace('src="','src="./static/'+dir+'/'+id+'/')
         fl.write(data)
+    db = sqlite3.connect('web_data.db')
+    title = request.form.get('title') 
+    desciption = request.form.get('desciption')
+
+    db.execute("INSERT INTO "+dir+" (id,title,desciption) VALUES ((?),(?),(?)) ",(id, title , desciption))
+    db.commit()
     return redirect('/admin_control')
 @app.route('/logout', methods = ['POST'])
 def logout():
@@ -117,35 +159,24 @@ def logout():
     return redirect('/')
 
 @app.route('/admin_control', methods = ['GET'])
+@login_required
 def admin_control():
     return render_template("admin_control.html")
 
 ########################################### done 
-@app.route('/bot')
-def bot():
 
-    return render_template("blog.html")
 @app.route('/cultures')
+@app.route('/articles')
 def culture():
+    db = sqlite3.connect('web_data.db')
+    cursor = db.execute("SELECT * FROM cultures ORDER BY date DESC LIMIT 10")
+    rows = cursor.fetchall()
+    return render_template("articles.html",row=rows,dir=request.path)
 
-    return render_template("cultures.html")
 @app.route('/photography')
-def hotography():
+def photography():
 
     return render_template("mystories.html")
-@app.route('/articles')
-def articles():
-
-    return render_template("articles.html")
-
-
-
-
-    
-
-
-
-
 
 @app.route('/<page>',methods = ['GET']) 
 @app.route('/<page>/<id>',methods = ['GET']) 
@@ -154,8 +185,10 @@ def page(page,id="empty"):
         parent_dir = "./static/"+page
         path = os.path.join(parent_dir, id) 
         path = os.path.join(path, "index.txt") 
+        data=""
         with open( path  ,'r') as f:
-            data=f.read()
+            for line in f.readlines():
+                data +=  line.strip().decode("utf-8")
         return render_template(page[:-1]+".html",data=data)
     else:
         return render_template("error.html", top=403, bottom="page don't exist",url=request.path),403 
