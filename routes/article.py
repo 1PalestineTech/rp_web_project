1d9f1b7289810a4a1ec506e7ab76e9066f066a44
Alitigui <alitighiouart2001@gmail.com>
2025-10-19 16:58:51 +0100

    articles template

---
+++ routes/article.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template
+
+article_bp = Blueprint('article', __name__)
+
+
+@article_bp.route('/articles')
+def myths():
+    return render_template("articles.html",title="Articles",
+                           message="At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum")
